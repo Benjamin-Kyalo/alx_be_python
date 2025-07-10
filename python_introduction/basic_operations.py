6be39108a5dd1d05da66dfcec8fbdf43d73befd0
shariz <benjahshariz@gmail.com>
2025-07-10 18:15:12 +0300

    simple arithmetic operations
    Adds a Python script that sets two numbers and prints their sum, difference, and product

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# Declaring two numbers
+number1 = 10
+number2 = 5
+
+# Addition
+sum = number1 + number2
+print("Addition of 10 and 5 is", sum)
+
+# Subtraction
+difference = number1 - number2
+print("Subtraction of 10 and 5 is", difference)
+
+# Multiplication
+product = number1 * number2
+print("Multiplication of 10 and 5 is", product)
